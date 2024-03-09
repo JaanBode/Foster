c70ad9b4688b7ffe9cf6be62b5f06b778ce40a61
Jaçanã Ferreira da Silva <67348567+JacanaFSilva@users.noreply.github.com>
2024-03-09 12:30:11 -0300

    Update page-stores.js
    atualizado const map da página page-store.js para coordenadas de Aracaju

--- public/scripts/page-stores.js
+++ public/scripts/page-stores.js
@@ -1,5 +1,5 @@
 //create map
-const map = L.map('mapid').setView([-26.2258244,-52.6700678], 15);
+const map = L.map('mapid').setView([-10.9439441,-37.0559183], 15);
 
 //create and add tileLayer
 L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png')

e484bd006378c5a213e38054bd0a643d1a536a8d
Jaçanã Ferreira da Silva <67348567+JacanaFSilva@users.noreply.github.com>
2024-03-09 01:29:20 -0300

    Add files via upload
    Arquivos para substituição na pasta principal

---
+++ Templates/Eng Software/public/scripts/page-create-store.js
@@ -0,0 +1,94 @@
+//create map
+const map = L.map('mapid').setView([-25.7046452,-53.1001927], 15)
+
+//create and add tileLayer
+L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png')
+.addTo(map)
+
+//create icon
+const icon = L.icon({
+    iconUrl: "/images/map-marker.svg",
+    iconSize: [58, 68],
+    iconAnchor: [29, 68]
+})
+
+//create and add marker
+let marker;
+map.on('click', (event) => {
+    const lat = event.latlng.lat;
+    const lng = event.latlng.lng;
+
+    document.querySelector('[name=lat]').value = lat;
+    document.querySelector('[name=lng]').value = lng;
+
+    //remove icon
+    marker && map.removeLayer(marker);
+
+    // add icon layer
+    marker = L.marker([lat, lng], {icon}).addTo(map)
+
+})
+
+// upload do campo de fotos
+function addPhotoField() {
+    //pegar o container de fotos
+    const container = document.querySelector('#images')
+    //pegar o container para duplicar .new-image
+    const fieldsContainer = document.querySelectorAll('.new-upload')
+    // realizar clone da ultima imagem adicionada
+    const newFieldContainer = fieldsContainer[fieldsContainer.length - 1].cloneNode(true)
+
+    //verificar se esta vazio, se sim, nao adicionar o container
+    const input = newFieldContainer.children[0]
+
+    if(input.value == "") {
+        return
+    }
+    // limpar o campo antes de adicionar o proximo container
+    input.value = ""
+
+    // adicionar o clone ao container de #images
+    container.appendChild(newFieldContainer)
+}
+
+function deleteField(event){
+    const span = event.currentTarget
+
+    const fieldsContainer = document.querySelectorAll('.new-upload')
+
+    if(fieldsContainer.length <= 1) {
+        // limpar valor do campo
+        span.parentNode.children[0].value = ""
+        return
+    }
+
+    // deletar o campo
+    span.parentNode.remove();
+
+
+}
+
+// selecionar o "sim" e "não"
+function toggleSelect(event) { 
+
+    document.querySelectorAll('.button-select button')
+    .forEach(button => button.classList.remove('active'))
+
+    //colocar a class .active nesse botao clicado
+    const button = event.currentTarget
+    button.classList.add('active')
+
+    // atualizar o input hidden com o valor selecionado
+    const input = document.querySelector('[name="open_on_weekends"]')
+
+    input.value = button.dataset.value
+}
+
+function validate(event){
+    const latlng = document.querySelector('input[type="hidden"]');
+    if(latlng.value ==""){
+        event.preventDefault()
+        alert('Selecione um ponto no mapa')
+    }
+    
+}

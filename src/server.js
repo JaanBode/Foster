b7b000a34563e438295c6220e4b92ac4d58b3b1f
Jaçanã Ferreira da Silva <67348567+JacanaFSilva@users.noreply.github.com>
2024-12-16 22:11:58 -0300

    Add files via upload

--- src/server.js
+++ src/server.js
@@ -2,19 +2,28 @@
 const express = require('express');
 const path = require('path');
 const pages = require('./pages.js');
-const port = 3000;
+const swaggerUi = require('swagger-ui-express');
+const swaggerDocument = require('./swagger.json');
 
 //iniciando o express
-const server = express()
+const port = 3000;
+const server = express();
+
 
 server
-//utilizar body do require
+// Configurando o Swagger UI para servir a documentação da API
+.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerDocument))
+
+//utilizar body do request
 .use(express.urlencoded({extended: true}))
+
 // utilizando arquivos estaticos
 .use(express.static('public'))
+
 //configurar template engine
 .set('views', path.join(__dirname, "views"))
 .set('view engine', 'hbs')
+
 //criar uma rota
 .get('/', pages.index)
 .get('/store', pages.store)
@@ -23,4 +32,7 @@ server
 .post('/save-store', pages.saveStore)
 
 //ligar o servidor
-server.listen(process.env.PORT || port)
+server.listen(port, () => {
+    console.log(`Servidor rodando em http://localhost:${port}`);
+    console.log(`Documentação disponível em http://localhost:${port}/api-docs`);
+});
